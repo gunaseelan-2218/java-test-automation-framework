f04e58a6b548cde8280648e8960d82dce998b222
krishnangun <gunavk0018@gmail.com>
2026-04-05 12:47:02 +0530

    added grid execution

--- src/test/java/com/test/framwork/cucumber/manager/DriverManager.java
+++ src/test/java/com/test/framwork/cucumber/manager/DriverManager.java
@@ -1,5 +1,6 @@
 package com.test.framwork.cucumber.manager;
 
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
@@ -10,24 +11,24 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.slf4j.Logger;
 
 import com.test.framwork.cucumber.constants.Constants;
 import com.test.framwork.cucumber.exception.TestException;
+import com.test.framwork.cucumber.utils.ConfigReader;
 import com.test.framwork.cucumber.utils.LoggerUtil;
 
 /**
- * DriverManager handles WebDriver lifecycle management
- * Supports cross-browser testing (Chrome, Firefox, Edge, Safari)
- * Provides static methods for driver initialization and cleanup
- * Uses ThreadLocal to support parallel test execution
- * Throws TestException for initialization failures
+ * DriverManager handles WebDriver lifecycle management Supports cross-browser
+ * testing (Chrome, Firefox, Edge, Safari) Provides static methods for driver
+ * initialization and cleanup Uses ThreadLocal to support parallel test
+ * execution Throws TestException for initialization failures
  * 
- * Browser Selection:
- * - Default: Chrome
- * - Override via: -Dbrowser=firefox (or edge, safari)
+ * Browser Selection: - Default: Chrome - Override via: -Dbrowser=firefox (or
+ * edge, safari)
  */
 public class DriverManager {
 
@@ -44,10 +45,11 @@ public class DriverManager {
 	}
 
 	/**
-	 * Initialize WebDriver instance based on browser type
-	 * Browser can be specified via system property: -Dbrowser=browserName
-	 * Supported browsers: chrome, firefox, edge, safari
-	 * Default: chrome
+	 * Initialize WebDriver instance based on browser type Supports both local and
+	 * Selenium Grid execution Browser can be specified via system property:
+	 * -Dbrowser=browserName Grid usage via: -DseleniumGrid=true and
+	 * -DgridURL=http://localhost:4444/wd/hub Supported browsers: chrome, firefox,
+	 * edge, safari Default: chrome, local execution
 	 * 
 	 * @throws TestException if driver initialization fails
 	 */
@@ -55,13 +57,15 @@ public class DriverManager {
 	public static void initDriver() {
 		try {
 			String browserName = System.getProperty("browser", Constants.DEFAULT_BROWSER).toLowerCase();
-			logger.info("Initializing WebDriver for browser: {}", browserName.toUpperCase());
+			boolean useGrid = Boolean.parseBoolean(System.getProperty("usegrid", "false"));
+			String gridURL = ConfigReader.getProperty("gridUrl");
 
-			WebDriver webDriver = createDriver(browserName);
+			logger.info("Initializing WebDriver for browser: {} | Grid Mode: {} ", browserName.toUpperCase(), useGrid);
+
+			WebDriver webDriver = createDriver(browserName, useGrid, gridURL);
 
 			// Set implicit wait
-			webDriver.manage().timeouts()
-				.implicitlyWait(Duration.ofSeconds(Constants.IMPLICIT_WAIT_SECONDS));
+			webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Constants.IMPLICIT_WAIT_SECONDS));
 			logger.debug("Implicit wait set to {} seconds", Constants.IMPLICIT_WAIT_SECONDS);
 
 			// Maximize window
@@ -70,7 +74,8 @@ public class DriverManager {
 
 			// Store in ThreadLocal
 			driver.set(webDriver);
-			logger.info("✓ WebDriver initialized successfully for browser: {}", browserName.toUpperCase());
+			logger.info("✓ WebDriver initialized successfully for browser: {} | Mode: {}", browserName.toUpperCase(),
+					useGrid ? "GRID" : "LOCAL");
 
 		} catch (TestException e) {
 			logger.error("Failed to initialize WebDriver", e);
@@ -83,13 +88,98 @@ public class DriverManager {
 	}
 
 	/**
+	 * Create WebDriver instance based on browser type and execution mode Supports
+	 * both local and Selenium Grid execution
+	 * 
+	 * @param browserName - browser type (chrome, firefox, edge, safari)
+	 * @param useGrid     - if true, creates RemoteWebDriver for Grid; else local
+	 *                    WebDriver
+	 * @param gridURL     - Selenium Grid hub URL (required when useGrid=true)
+	 * @return WebDriver instance
+	 * @throws TestException if browser type is not supported or Grid connection
+	 *                       fails
+	 */
+	private static WebDriver createDriver(String browserName, boolean useGrid, String gridURL) {
+		logger.debug("Creating WebDriver instance for browser: {} | Grid: {}", browserName, useGrid);
+
+		if (useGrid) {
+			return createRemoteDriver(browserName, gridURL);
+		} else {
+			return createLocalDriver(browserName);
+		}
+	}
+
+	/**
+	 * Create RemoteWebDriver for Selenium Grid execution
+	 * 
+	 * @param browserName - browser type
+	 * @param gridURL     - Selenium Grid hub URL
+	 * @return RemoteWebDriver instance
+	 * @throws TestException if browser not supported or Grid connection fails
+	 */
+	private static RemoteWebDriver createRemoteDriver(String browserName, String gridURL) {
+		try {
+			URL hubURL = new URL(gridURL);
+			logger.info("Connecting to Selenium Grid at: {}", gridURL);
+
+			RemoteWebDriver remoteDriver = null;
+
+			switch (browserName.toLowerCase()) {
+			case Constants.BROWSER_CHROME:
+				logger.info("Launching RemoteWebDriver for Chrome on Grid");
+				ChromeOptions chromeOptions = new ChromeOptions();
+				chromeOptions.addArguments("--disable-gpu", "--window-size=1920,1080");
+				chromeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+				remoteDriver = new RemoteWebDriver(hubURL, chromeOptions);
+				break;
+
+			case Constants.BROWSER_FIREFOX:
+				logger.info("Launching RemoteWebDriver for Firefox on Grid");
+				FirefoxOptions firefoxOptions = new FirefoxOptions();
+				firefoxOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+				remoteDriver = new RemoteWebDriver(hubURL, firefoxOptions);
+				break;
+
+			case Constants.BROWSER_EDGE:
+				logger.info("Launching RemoteWebDriver for Edge on Grid");
+				EdgeOptions edgeOptions = new EdgeOptions();
+				edgeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+				remoteDriver = new RemoteWebDriver(hubURL, edgeOptions);
+				break;
+
+			case Constants.BROWSER_SAFARI:
+				logger.info("Launching RemoteWebDriver for Safari on Grid");
+				SafariOptions safariOptions = new SafariOptions();
+				safariOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+				remoteDriver = new RemoteWebDriver(hubURL, safariOptions);
+				break;
+
+			default:
+				String errorMsg = String.format(
+						"Unsupported browser type on Grid: %s. Supported: chrome, firefox, edge, safari", browserName);
+				logger.error(errorMsg);
+				throw new TestException(errorMsg);
+			}
+
+			logger.info("✓ RemoteWebDriver connected to Grid: {} | Browser: {}", gridURL, browserName.toUpperCase());
+			return remoteDriver;
+
+		} catch (Exception e) {
+			String errorMsg = String.format("Failed to create RemoteWebDriver for Grid. URL: %s, Browser: %s", gridURL,
+					browserName);
+			logger.error(errorMsg, e);
+			throw new TestException(errorMsg, e);
+		}
+	}
+
+	/**
 	 * Create local WebDriver instance with proper browser options
 	 * 
 	 * @param browserName - browser type (chrome, firefox, edge, safari)
 	 * @return WebDriver instance
 	 * @throws TestException if browser type is not supported
 	 */
-	private static WebDriver createDriver(String browserName) {
+	private static WebDriver createLocalDriver(String browserName) {
 		logger.debug("Creating WebDriver instance for browser: {}", browserName);
 
 		switch (browserName.toLowerCase()) {
@@ -128,8 +218,7 @@ public class DriverManager {
 	}
 
 	/**
-	 * Quit WebDriver and cleanup resources
-	 * Safely removes driver from ThreadLocal
+	 * Quit WebDriver and cleanup resources Safely removes driver from ThreadLocal
 	 */
 	public static void quitDriver() {
 		try {
